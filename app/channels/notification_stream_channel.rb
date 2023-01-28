65c00a76982e8e44541a06803e121965037cc9ff
Nicholas Levenson <nicklevenson@gmail.com>
2023-01-28 12:15:22 -0800

    add read for notification stream

--- app/channels/notification_stream_channel.rb
+++ app/channels/notification_stream_channel.rb
@@ -8,5 +8,17 @@ class NotificationStreamChannel < ApplicationCable::Channel
     ActionCable.server.broadcast("notification_stream_#{user.id}", notifications)
   end
 
+  def mark_read(data)
+    id = params[:id]
+    user = User.find(id)
+    notification = user.notifications.find(data['notification_id'])
+    notification.update!(read: true)
+    notifications = user.notifications.map do |notification|
+      NotificationSerializer.new(notification, current_user: user)
+    end
+
+    ActionCable.server.broadcast("notification_stream_#{user.id}", notifications)
+  end
+
   def unsubscribed; end
 end
